226e5f5b7fa6c62fa8a775583c7d5fa35f3c50e1
Manuel Rivallo <mrivallo@hotmail.com>
2022-05-24 13:39:34 +0200

    Coloreando tabla de pedidos

---
+++ src/marketbeezup/controladores/TablaPedidosCellRenderer.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package marketbeezup.controladores;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+import marketbeezup.modelos.Articulo;
+import marketbeezup.modelos.Pedido;
+
+/**
+ *
+ * @author Manolo
+ */
+public class TablaPedidosCellRenderer extends DefaultTableCellRenderer {
+
+    List<Articulo> articulos;
+    List<Pedido> pedidos;
+
+    public TablaPedidosCellRenderer(List<Pedido> pedidos) {
+        this.pedidos = pedidos;
+        articulos = new ArrayList<>();
+        for (Pedido pedido : pedidos) {
+            articulos.addAll(pedido.getArticulos());
+        }
+    }
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        String idPedidoAnterior = null;
+        String marketplaceAnterior = null;
+        String idPedidoSiguiente = null;
+        String marketplaceSiguiente = null;
+        if (articulos.get(row).getEstado().equals("CANCELADO")) {
+            this.setForeground(Color.red);
+        } else {
+            if (row < articulos.size() - 1) {
+                idPedidoSiguiente = articulos.get(row + 1).getIdPedido();
+                marketplaceSiguiente = articulos.get(row + 1).getMarketplace();
+            }
+            if (row > 0) {
+                idPedidoAnterior = articulos.get(row - 1).getIdPedido();
+                marketplaceAnterior = articulos.get(row - 1).getMarketplace();
+            }
+            if (articulos.get(row).getIdPedido().equals(idPedidoAnterior)
+                    & articulos.get(row).getMarketplace().equals(marketplaceAnterior)) {
+                this.setForeground(Color.orange);
+            } else {
+                if (articulos.get(row).getIdPedido().equals(idPedidoSiguiente)
+                        & articulos.get(row).getMarketplace().equals(marketplaceSiguiente)) {
+                    this.setForeground(Color.orange);
+                } else {
+                    this.setForeground(Color.white);
+                }
+            }
+        }
+        /*String idPedido;
+        String marketplace;
+        if ((row + 1) < articulos.size()) {
+            
+                if (row > 0) {
+                    if (articulos.get(row).getIdPedido().equals(articulos.get(row - 1).getIdPedido()) & 
+                            articulos.get(row).getMarketplace().equals(articulos.get(row - 1).getMarketplace())) {
+                        this.setForeground(Color.orange);
+                    } else {
+                        this.setForeground();
+                    }
+                }
+                this.setForeground(Color.white);
+            }
+        } else {
+            this.setForeground(Color.white);
+        }
+        /*
+        switch (String.valueOf(table.getValueAt(row, 15))) {
+            case "Novedad":
+                this.setBackground(Color.white);
+                this.setForeground(Color.green);
+                break;
+            case "Actual":
+                this.setBackground(Color.white);
+                this.setForeground(Color.black);
+                break;
+            case "Preobsoleto":
+                this.setBackground(Color.white);
+                this.setForeground(Color.red);
+                break;
+            case "Obsoleto":
+                this.setBackground(Color.red);
+                this.setForeground(Color.white);
+                break;
+            default:
+                this.setBackground(Color.black);
+                this.setForeground(Color.white);
+        }
+        this.setToolTipText(String.valueOf(table.getValueAt(row, column)));
+         */
+        return this;
+    }
+}
